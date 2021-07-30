5b52aa026ad6ed6a93791afc1c0b82a6f60fbd4f
Itai Pendler <212519425@idf.il>
2021-07-30 11:40:34 +0300

    rearrange code and remove blank todos and not accept blank todos
    Signed-off-by: Itai Pendler <212519425@idf.il>

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.test
 import android.content.Context
 import android.os.Bundle
 import android.view.View
-import android.view.animation.*
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import android.view.animation.Animation.AnimationListener
+import android.view.animation.DecelerateInterpolator
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,30 +16,47 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        val preferences = this.getPreferences(Context.MODE_PRIVATE);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val helloWorldTextView = findViewById<TextView>(R.id.hello_world_text)
+        val editText = findViewById<EditText>(R.id.edittext)
+        val enterButton = findViewById<Button>(R.id.enterButton)
+        val nextButton = findViewById<Button>(R.id.nextButton)
+        val clearButton = findViewById<Button>(R.id.clearButton)
 
-        val fadeIn = AlphaAnimation(0f, 1f);
-        fadeIn.interpolator = DecelerateInterpolator(); //add this
-        fadeIn.duration = 500;
-        val fadeOut = AlphaAnimation(1f, 0f);
-        fadeOut.interpolator = AccelerateInterpolator(); //and this
-        fadeOut.duration = 500;
-        var index = 0;
-        val stringArr = mutableSetOf(
-            getString(R.string.app_name),
-            getString(R.string.appbar_scrolling_view_behavior),
-            getString(R.string.fab_transformation_sheet_behavior)
-        );
-        val helloWorldTextView = findViewById<TextView>(R.id.hello_world_text);
-        val editText = findViewById<EditText>(R.id.edittext);
-        val enterButton = findViewById<Button>(R.id.enterButton);
+        val preferences = this.getPreferences(Context.MODE_PRIVATE)
+        val prefString = preferences.getString(getString(R.string.string_set_key), "") ?: ""
+        val stringArr = mutableListOf<String>()
+        stringArr.addAll(0, prefString.split(','))
+        if (stringArr.elementAt(0).isEmpty()) {
+            stringArr.removeAt(0)
+        }
+        println(stringArr.toString())
+
+        val fadeIn = AlphaAnimation(0f, 1f)
+        fadeIn.interpolator = DecelerateInterpolator() //add this
+        fadeIn.duration = 500
+        val fadeOut = AlphaAnimation(1f, 0f)
+        fadeOut.interpolator = AccelerateInterpolator() //and this
+        fadeOut.duration = 500
+        var index = 0
+
+
         enterButton.setOnClickListener {
-            val tempText = editText.text.toString();
-            stringArr.add(tempText);
-            preferences.edit().putStringSet(getString(R.string.string_set_key), stringArr).apply();
-            editText.text.clear();
+            val tempText = editText.text.toString()
+            if (!tempText.isEmpty()) {
+                stringArr.add(tempText)
+                println(tempText)
+                println(stringArr)
+                with(preferences.edit()) {
+                    val stringToSave = stringArr.toTypedArray().joinToString(",")
+                    putString(getString(R.string.string_set_key), stringToSave)
+                    apply()
+                }
+            }
+            println(stringArr.toString())
+            println(tempText)
+            editText.text.clear()
         }
         fadeOut.setAnimationListener(object : AnimationListener {
             override fun onAnimationStart(animation: Animation) {}
@@ -45,22 +65,31 @@ class MainActivity : AppCompatActivity() {
                 if (index == stringArr.size) {
                     index = 0
                 }
-                helloWorldTextView.text = stringArr.elementAt(index++);
-                helloWorldTextView.startAnimation(fadeIn);
+                val stringToShow = stringArr[index++]
+                helloWorldTextView.text = stringToShow
+
+                helloWorldTextView.startAnimation(fadeIn)
             }
         })
         fadeIn.setAnimationListener(object : AnimationListener {
             override fun onAnimationStart(animation: Animation) {}
             override fun onAnimationRepeat(animation: Animation) {}
             override fun onAnimationEnd(animation: Animation) {
-                helloWorldTextView.visibility = View.VISIBLE;
+                helloWorldTextView.visibility = View.VISIBLE
             }
         })
-        helloWorldTextView.startAnimation(fadeIn);
+        helloWorldTextView.startAnimation(fadeIn)
 
-        helloWorldTextView.setOnClickListener(View.OnClickListener {
-            helloWorldTextView.startAnimation(fadeOut);
-        });
+        helloWorldTextView.setOnClickListener {
+            helloWorldTextView.startAnimation(fadeOut)
+        }
 
+        nextButton.setOnClickListener {
+            helloWorldTextView.startAnimation(fadeOut)
+        }
+        clearButton.setOnClickListener {
+            preferences.edit().remove(getString(R.string.string_set_key)).apply()
+            stringArr.clear()
+        }
     }
 }
